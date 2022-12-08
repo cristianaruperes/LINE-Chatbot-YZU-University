d96a63318490fdb879ab910dc9c7e5cbc6534365
YOURMAN\crist <cristianaruperes12@gmail.com>
2022-12-08 14:53:49 +0800

    update model

--- model.py
+++ model.py
@@ -1,19 +1,19 @@
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-def ask(q):
+def ask(q):#
     import openai
-    openai.api_key = "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"
+    openai.api_key = "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"
                    
     response = openai.Completion.create(
-        model="davinci:ft-yuan-ze-university-2022-12-08-04-51-24",
-        prompt=q,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", "END"]
+    model="text-curie-001",
+    prompt=q,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["END"]
 )                     
     story = response['choices'][0]['text'] 
     return story
